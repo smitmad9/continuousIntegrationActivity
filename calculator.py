719993153acc795876d8024c6a09446b4bc5f766
Maddie Smith <smitmad9@oregonstate.edu>
2021-03-13 17:04:56 -0800

    Add addition and subtraction

---
+++ calculator.py
@@ -0,0 +1,14 @@
+# 1. Implement addition and subtraction, commit to github repo
+
+
+def addition(x, y):
+    return x + y
+
+def subtraction(x, y):
+    return x - y
+
+#def multiplication(x, y):
+ #   return x * y
+
+#def division(x, y):
+ #   return float(x) / float(y)
