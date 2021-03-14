2963df33273f20d1326914cb79a0278d6b7cc757
Maddie Smith <smitmad9@oregonstate.edu>
2021-03-13 17:14:55 -0800

    Fixed linting errors

--- calculator.py
+++ calculator.py
@@ -1,14 +1,14 @@
 # 1. Implement addition and subtraction, commit to github repo
 
-
 def addition(x, y):
-    return x + y
+    return float(x) + float(y)
+
 
 def subtraction(x, y):
-    return x - y
+    return float(x) - float(y)
 
-#def multiplication(x, y):
- #   return x * y
+# def multiplication(x, y):
+#     return x * y
 
-#def division(x, y):
- #   return float(x) / float(y)
+# def division(x, y):
+#     return float(x) / float(y)
